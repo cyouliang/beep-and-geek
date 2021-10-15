2ed7042163904cadec91de26ca40bd734a16a81d
cyouliang <youliang1997@hotmail.com>
2021-10-15 17:53:27 +0800

    updated product.php and sort some misc

--- product.php
+++ product.php
@@ -1,10 +1,139 @@
-<?php
-$product = $_GET['product'];
-include "script/connectDB.php";
-$query = "SELECT * FROM Products WHERE ProductName = '$product'";
-$result = mysqli_query($conn, $query);
-$row = mysqli_fetch_array($result);
-echo $row['ProductName'].'<br>';
-mysqli.close($conn);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="HandheldFriendly" content="true">
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>Beep and Geek</title>
+    <?php
+        $product = $_GET['product'];
+        include "script/connectDB.php";
+        $query = "SELECT * FROM Products WHERE ProductName = '$product'";
+        $result = mysqli_query($conn, $query);
+        $rowcount = mysqli_num_rows($result);
+        $rows = array();
+        while($row = mysqli_fetch_array($result))
+        {
+            $rows[] = $row;
+        }
 
-?>
+        echo '<pre>'. var_export($rows,true) .'</pre>';
+        mysqli_close($conn);
+    ?>
+</head>
+
+
+<body>
+    <header>
+        <a href="index2.html"><img src="../beep-and-geek/img/logo.png" alt="maincatalog"></a>
+    </header>
+
+    <div id="topnavbar">
+        <nav>
+            <b>
+                <a href="index2.html">Home</a>
+                <a href="catalogue.php">All</a>
+                <a href="catalogue.php?browseby=phone">Phones</a>
+                <a href="catalogue.php?browseby=laptop">Laptops</a>
+                <a href="catalogue.php?browseby=earphone">Earphones</a>
+            </b>
+           </nav>
+    </div>
+
+    <div id="pagelayout">
+        <div id="rightcolumn_2">
+        
+        <!-- Slideshow container -->
+            <div class="slideshow-container">
+                <!-- Full-width images with number and caption text -->
+                
+
+                <?php
+                    foreach($rows as $row){
+                        $image = $row['VariantImage'];
+                        $name = $row['ProductName'];
+                        $color = $row['Color'];
+                        $price = $row['Price'];
+                        $productid = $row['ProductID'];
+
+                        echo
+                        '
+                        <div class="mySlides fade">
+                            <div class="numbertext">'.$productid.' / '.$rowcount.'</div>
+                            <img src=" img/products/'.$image.'" style="width:100%">
+                            <div class="text"><strong>'.$name.'<br>'.$color.'<br> $'.$price.'</strong></div>
+                        </div>
+                        ';
+                    }
+                
+                ?>
+                
+                <!-- <div class="mySlides fade">
+                    <div class="numbertext">2 / 4</div>
+                    <img src="../beep-and-geek/img/products/iphone13pro-sierrablue.png" style="width:100%">
+                    <div class="text"><strong>iPhone 13 Pro <br> Sierra Blue</strong></div>
+                </div>
+                
+                <div class="mySlides fade">
+                    <div class="numbertext">3 / 4</div>
+                    <img src="../beep-and-geek/img/products/iphone13pro-gold.png" style="width:100%">
+                    <div class="text"><strong>iPhone 13 Pro <br> Gold</strong></div>
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="numbertext">4 / 4</div>
+                    <img src="../beep-and-geek/img/products/iphone13pro-silver.png" style="width:100%">
+                    <div class="text"><strong>iPhone 13 Pro <br> Silver</strong></div>
+                </div> -->
+                
+                <!-- Next and previous buttons -->
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                
+                <br>
+
+                <!-- The dots/circles -->
+                <div style="text-align:center">
+                <?php 
+                switch($rowcount)
+                {   
+                    case("2")?>
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span> 
+                <?php break; 
+                    case("3")?>
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span>
+                <?php break; 
+                    case("4")?>
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span>
+                    <span class="dot" onclick="currentSlide(4)"></span>  
+                <?php break; 
+                    case("5")
+                   ?>
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span>
+                    <span class="dot" onclick="currentSlide(4)"></span>
+                    <span class="dot" onclick="currentSlide(5)"></span> 
+                <?php break;
+                } ?> 
+                
+                </div>
+            </div>
+        </div>        
+        
+        <script type="text/javascript" src="promaxslideshow.js"></script>
+
+    </div>
+    
+</body>
+</html>
+
+
+
--- script/insertItems.php
+++ script/insertItems.php
@@ -5,28 +5,35 @@ $sql =
 ('Iphone 13', 'phone', 'iphone13-red.png'),
 ('Iphone 13 Mini', 'phone', 'iphone13mini-blue.png'),
 ('Iphone 13 Pro', 'phone', 'iphone13pro-gold.png'),
-('Iphone 13 Pro Max', 'phone', 'iphone13promax-silver.png');
+('Iphone 13 Pro Max', 'phone', 'iphone13promax-silver.png'),
+('Macbook Air', 'laptop', 'macbookair-gold.png'),
+('Macbook Pro 13', 'laptop', 'macbookpro13-silver.png');
 
 
 INSERT INTO Products VALUES
 ('A01', 'Iphone 13', 'iphone13-black.png', 1299, 3, 'Black'),
-('A02', 'Iphone 13', 'iphone13-blue.png', 1349, 1, 'Blue'),
+('A02', 'Iphone 13', 'iphone13-blue.png', 1299, 1, 'Blue'),
 ('A03', 'Iphone 13', 'iphone13-pink.png', 1299, 2, 'Pink'),
-('A04', 'Iphone 13', 'iphone13-red.png', 1349, 0, 'Red'),
+('A04', 'Iphone 13', 'iphone13-red.png', 1299, 0, 'Red'),
 ('A05', 'Iphone 13', 'iphone13-starlight.png', 1299, 2, 'Starlight'),
 ('B01', 'Iphone 13 Mini', 'iphone13mini-black.png', 1149, 3, 'Black'),
 ('B02', 'Iphone 13 Mini', 'iphone13mini-blue.png', 1149, 2, 'Blue'),
-('B03', 'Iphone 13 Mini', 'iphone13mini-pink.png', 1199, 1, 'Pink'),
-('B04', 'Iphone 13 Mini', 'iphone13mini-red.png', 1179, 1, 'Red'),
+('B03', 'Iphone 13 Mini', 'iphone13mini-pink.png', 1149, 1, 'Pink'),
+('B04', 'Iphone 13 Mini', 'iphone13mini-red.png', 1149, 1, 'Red'),
 ('B05', 'Iphone 13 Mini', 'iphone13mini-starlight.png', 1149, 1, 'Starlight'),
 ('C01', 'Iphone 13 Pro', 'iphone13pro-gold.png', 1649, 3, 'Gold'),
-('C02', 'Iphone 13 Pro', 'iphone13pro-graphite.png', 1699, 2, 'Graphite'),
-('C03', 'Iphone 13 Pro', 'iphone13pro-sierrablue.png', 1699, 1, 'Sierra Blue'),
-('C04', 'Iphone 13 Pro', 'iphone13pro-silver.png', 1699, 1, 'Silver'),
+('C02', 'Iphone 13 Pro', 'iphone13pro-graphite.png', 1649, 2, 'Graphite'),
+('C03', 'Iphone 13 Pro', 'iphone13pro-sierrablue.png', 1649, 1, 'Sierra Blue'),
+('C04', 'Iphone 13 Pro', 'iphone13pro-silver.png', 1649, 1, 'Silver'),
 ('D01', 'Iphone 13 Pro Max', 'iphone13promax-gold.png', 1799, 0, 'Gold'),
 ('D02', 'Iphone 13 Pro Max', 'iphone13promax-graphite.png', 1799, 0, 'Graphite'),
 ('D03', 'Iphone 13 Pro Max', 'iphone13promax-sierrablue.png', 1799, 0, 'Sierra Blue'),
-('D04', 'Iphone 13 Pro Max', 'iphone13promax-silver.png', 1799, 0, 'Silver');
+('D04', 'Iphone 13 Pro Max', 'iphone13promax-silver.png', 1799, 0, 'Silver'),
+('E01', 'Macbook Air', 'macbookair-gold.png', 1449, 10, 'Gold'),
+('E02', 'Macbook Air', 'macbookair-silver.png', 1449, 10, 'Silver'),
+('E03', 'Macbook Air', 'macbookair-spacegrey.png', 1449, 10, 'Space Grey'),
+('F01', 'Macbook Pro 13', 'macbookpro13-silver.png', 1849, 10, 'Silver'),
+('F02', 'Macbook Pro 13', 'macbookpro13-spacegrey.png', 1849, 10, 'Space Grey');
 ";
 
 
