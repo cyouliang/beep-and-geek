bab4da90f99e30fbb174b4ab79c0af93a57a2730
Neo Zhen Ting <neozt99@gmail.com>
2021-10-24 17:25:46 +0800

    bare minimum styling for checkout pages

--- checkout.php
+++ checkout.php
@@ -10,12 +10,13 @@ $all_available = true; //Tracks if all items have enough stock
 mysqli_close($conn);
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>B&G - Shopping cart</title>
-    <meta charset="UTF-8">
+    <title>B&G - Cart</title>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="HandheldFriendly" content="true">
@@ -23,60 +24,96 @@ mysqli_close($conn);
 </head>
 
 <body>
-    <?php if ($_SESSION['recent_action'] == 'reduce_cart') : ?>
-        <p>Unavailable items have been removed</p>
-        <?php unset($_SESSION['recent_action']) ?>
-    <?php endif ?>
-    <p>You have <?= count($_SESSION['cart']) ?> items in your cart!<br></p>
+    <div id="container">
+        <header>
+            <a href="index3.html">
+                <div id="name">Beep&Geek</div>
+            </a>
+            <a href="loginpage.php">
+                <div id="login">
+                    <?php if (isset($_SESSION['valid_user'])) : ?>
+                        <?= $_SESSION['valid_user'] ?>
+                    <?php else : ?>
+                        Login
+                    <?php endif; ?>
+                </div>
+            </a>
+            <a href="checkout.php">
+                <div id="cart">Cart</div>
+            </a>
+        </header>
 
-    <?php if (!empty($products)) : ?>
-        Cart: <br>
-        <table border="1">
-            <tr>
-                <th>Product ID</th>
-                <th>Product</th>
-                <th>Color</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Subtotal</th>
-            </tr>
+        <div id="topnavbar">
+            <nav>
+                <b>
+                    <a href="index3.html">Home</a>
+                    <a href="catalogue.php">All</a>
+                    <a href="catalogue.php?browseby=phone">iPhones</a>
+                    <a href="catalogue.php?browseby=laptop">Mac</a>
+                    <a href="catalogue.php?browseby=earphone">Airpods</a>
+                </b>
+            </nav>
+        </div>
+        <div class="content">
+            <div id="pagelayout">
+                <div id="rightcolumn_2">
+                    <?php if ($_SESSION['recent_action'] == 'reduce_cart') : ?>
+                        <p>Unavailable items have been removed</p>
+                        <?php unset($_SESSION['recent_action']) ?>
+                    <?php endif ?>
+                    <p>You have <?= count($_SESSION['cart']) ?> items in your cart!<br></p>
 
-            <!-- php foreach loop embedded in html: https://stackoverflow.com/questions/10258345/php-simple-foreach-loop-with-html -->
-            <?php foreach ($products as $product) : ?>
-                <tr>
-                    <td align="center"><?= $product['ProductID']; ?></td>
-                    <td align="left"><?= $product['ProductName']; ?></td>
-                    <td align="left"><?= $product['Color']; ?></td>
-                    <td align="center"><?= $product['Price']; ?></td>
-                    <?php
-                    //Check if there is enough stock
-                    if ($product["Stock"] >= $product["Quantity"])
-                        echo '<td align="center"> ' . $product['Quantity'] . '</td>';
-                    else {
-                        $all_available = false;
-                        echo '<th align="center"> ' . $product['Quantity'] . '*</th>';
-                    }
-                    ?>
-                    <td align="center">$<?= number_format($product['Price'] * $product['Quantity']); ?></td>
-                </tr>
-            <?php endforeach; ?>
-            <th id="total-price" align="center" colspan="6">Total: $<?= number_format($total_price) ?></th>
-        </table>
+                    <?php if (!empty($products)) : ?>
+                        Cart: <br>
+                        <table border="1">
+                            <tr>
+                                <th>Product ID</th>
+                                <th>Product</th>
+                                <th>Color</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Subtotal</th>
+                            </tr>
 
-        <!-- Check if all items available before proceeding -->
-        <?php if ($all_available) : ?>
-            <form action="input_details.php">
-                <input type="submit" value="Proceed with transaction!">
-            </form>
-        <?php else : ?>
-            <p><b>*Sorry one or more items are not available</b></p>
-            <p><a href="script/clear_cart.php?clear=1" value="">Click here to clear cart</a></p>
-            <p><a href="script/clear_cart.php?clear=2" value="">Or click here to remove unavailable items</a></p>
-        <?php endif; ?>
+                            <!-- php foreach loop embedded in html: https://stackoverflow.com/questions/10258345/php-simple-foreach-loop-with-html -->
+                            <?php foreach ($products as $product) : ?>
+                                <tr>
+                                    <td align="center"><?= $product['ProductID']; ?></td>
+                                    <td align="left"><?= $product['ProductName']; ?></td>
+                                    <td align="left"><?= $product['Color']; ?></td>
+                                    <td align="center"><?= $product['Price']; ?></td>
+                                    <?php
+                                    //Check if there is enough stock
+                                    if ($product["Stock"] >= $product["Quantity"])
+                                        echo '<td align="center"> ' . $product['Quantity'] . '</td>';
+                                    else {
+                                        $all_available = false;
+                                        echo '<th align="center"> ' . $product['Quantity'] . '*</th>';
+                                    }
+                                    ?>
+                                    <td align="center">$<?= number_format($product['Price'] * $product['Quantity']); ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            <th id="total-price" align="center" colspan="6">Total: $<?= number_format($total_price) ?></th>
+                        </table>
 
-    <?php else : ?>
-        <a href="catalogue.php" alt="Catalogue">Click here to start filling it up!</a>
-    <?php endif; ?>
+                        <!-- Check if all items available before proceeding -->
+                        <?php if ($all_available) : ?>
+                            <form action="input_details.php">
+                                <input type="submit" value="Proceed with transaction!">
+                            </form>
+                        <?php else : ?>
+                            <p><b>*Sorry one or more items are not available</b></p>
+                            <p><a href="script/clear_cart.php?clear=1" value="">Click here to clear cart</a></p>
+                            <p><a href="script/clear_cart.php?clear=2" value="">Or click here to remove unavailable items</a></p>
+                        <?php endif; ?>
+
+                    <?php else : ?>
+                        <a href="catalogue.php" alt="Catalogue">Click here to start filling it up!</a>
+                    <?php endif; ?>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </body>
-
-</html>
--- completion_page.php
+++ completion_page.php
@@ -2,51 +2,87 @@
 session_start();
 require_once 'script/functions.php';
 $transaction = $_SESSION['transaction'];
-session_unset();
-session_destroy();
+unset($_SESSION['transaction']);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <title>Transaction complete!</title>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="HandheldFriendly" content="true">
     <link rel="stylesheet" href="stylesheet.css">
-    <title>Transaction complete</title>
+    <script type="text/javascript" src="validate.js"></script>
 </head>
 
 <body>
-    <p>Thank you <b><?= $transaction['cust_name'] ?></b></p>
-    <p>We have received your order!</p>
-    <p>Please keep the order number for future reference: <b><?= $transaction['orderid'] ?></b></p>
-    <p>Here is a summary of what you ordered</p>
-    <table border="1">
-        <tr>
-            <th>Product ID</th>
-            <th>Product</th>
-            <th>Color</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Subtotal</th>
-        </tr>
-        <?php foreach ($transaction['products'] as $product) : ?>
-            <tr>
-                <td align="center"><?= $product['ProductID']; ?></td>
-                <td align="left"><?= $product['ProductName']; ?></td>
-                <td align="left"><?= $product['Color']; ?></td>
-                <td align="center"><?= $product['Price']; ?></td>
-                <td align="center"><?= $product['Quantity']; ?></td>
-                <td align="center">$<?= number_format($product['Price'] * $product['Quantity']); ?></td>
-            </tr>
-        <?php endforeach; ?>
-        <tr>
-            <th id="total-price" align="right" colspan="6">Total: $<?= number_format(total_price($transaction['products'])) ?></th>
-        </tr>
-    </table>
-    <p>A copy of the receipt has been sent to your email at <b><?= $transaction['cust_email'] ?></b>
-</body>
+    <div id="container">
+        <header>
+            <a href="index3.html">
+                <div id="name">Beep&Geek</div>
+            </a>
+            <a href="loginpage.php">
+                <div id="login">
+                    <?php if (isset($_SESSION['valid_user'])) : ?>
+                        <?= $_SESSION['valid_user'] ?>
+                    <?php else : ?>
+                        Login
+                    <?php endif; ?>
+                </div>
+            </a>
+            <a href="checkout.php">
+                <div id="cart">Cart</div>
+            </a>
+        </header>
 
-</html>
+        <div id="topnavbar">
+            <nav>
+                <b>
+                    <a href="index3.html">Home</a>
+                    <a href="catalogue.php">All</a>
+                    <a href="catalogue.php?browseby=phone">iPhones</a>
+                    <a href="catalogue.php?browseby=laptop">Mac</a>
+                    <a href="catalogue.php?browseby=earphone">Airpods</a>
+                </b>
+            </nav>
+        </div>
+        <div class="content">
+            <div id="pagelayout">
+                <div id="rightcolumn_2">
+                    <p>Thank you <b><?= $transaction['cust_name'] ?></b></p>
+                    <p>We have received your order!</p>
+                    <p>Please keep the order number for future reference: <b><?= $transaction['orderid'] ?></b></p>
+                    <p>Here is a summary of what you ordered</p>
+                    <table border="1">
+                        <tr>
+                            <th>Product ID</th>
+                            <th>Product</th>
+                            <th>Color</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                        <?php foreach ($transaction['products'] as $product) : ?>
+                            <tr>
+                                <td align="center"><?= $product['ProductID']; ?></td>
+                                <td align="left"><?= $product['ProductName']; ?></td>
+                                <td align="left"><?= $product['Color']; ?></td>
+                                <td align="center"><?= $product['Price']; ?></td>
+                                <td align="center"><?= $product['Quantity']; ?></td>
+                                <td align="center">$<?= number_format($product['Price'] * $product['Quantity']); ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                        <tr>
+                            <th id="total-price" align="right" colspan="6">Total: $<?= number_format(total_price($transaction['products'])) ?></th>
+                        </tr>
+                    </table>
+                    <p>A copy of the receipt has been sent to your email at <b><?= $transaction['cust_email'] ?></b>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
