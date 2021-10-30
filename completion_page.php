a4710e6c035d2f3d6ff1643710b64e1b2c0b4a02
Neo Zhen Ting <neozt99@gmail.com>
2021-10-30 22:27:00 +0800

    Styled completion page

--- completion_page.php
+++ completion_page.php
@@ -31,6 +31,18 @@ mail($to, $subject, $message, $headers,'-ff32ee@localhost');
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="HandheldFriendly" content="true">
     <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="css/table.css">
+    <style>
+        .product-table tr:nth-last-child(2) td{
+            border-bottom: none;
+        }
+        .product-table {
+            margin-top: 10px; 
+            margin-bottom: 10px; 
+            margin-left: 0px; 
+            margin-right: 0px; 
+        }
+    </style>
     <script type="text/javascript" src="validate.js"></script>
 </head>
 
@@ -74,36 +86,37 @@ mail($to, $subject, $message, $headers,'-ff32ee@localhost');
         <div class="content">
             <div id="pagelayout">
                 <div id="rightcolumn_2">
-                    <p>Thank you <b><?= $transaction['cust_name'] ?></b></p>
-                    <p>We have received your order!</p>
-                    <p>Please keep the order number for future reference: <b><?= $transaction['orderid'] ?></b></p>
-                    <p>Here is a summary of what you ordered</p>
-                    <table border="1">
-                        <tr>
-                            <th>Product ID</th>
-                            <th>Product</th>
-                            <th>Color</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Subtotal</th>
-                        </tr>
-                        <?php foreach ($transaction['products'] as $product) : ?>
+                    <div class="completion-details">
+                        <p>Thank you <b><?= $transaction['cust_name'] ?></b>!</p>
+                        <p>We have received your order.</p>
+                        <p>Here is a summary of what you ordered:</p>
+                        <table class="product-table">
                             <tr>
-                                <td align="center"><?= $product['ProductID']; ?></td>
-                                <td align="left"><?= $product['ProductName']; ?></td>
-                                <td align="left"><?= $product['Color']; ?></td>
-                                <td align="center"><?= $product['Price']; ?></td>
-                                <td align="center"><?= $product['Quantity']; ?></td>
-                                <td align="center">$<?= number_format($product['Price'] * $product['Quantity'], 2); ?></td>
+                                <th>Product ID</th>
+                                <th>Product</th>
+                                <th>Color</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Subtotal</th>
                             </tr>
-                        <?php endforeach; ?>
-                        <tr>
-                            <th id="total-price" align="right" colspan="6">Total: $<?= number_format(total_price($transaction['products']),2) ?></th>
-                        </tr>
-                    </table>
-                    <p>A copy of the receipt has been sent to your email at <b><?= $transaction['cust_email'] ?></b>
+                            <?php foreach ($transaction['products'] as $product) : ?>
+                                <tr>
+                                    <td align="center"><?= $product['ProductID']; ?></td>
+                                    <td align="left"><?= $product['ProductName']; ?></td>
+                                    <td align="left"><?= $product['Color']; ?></td>
+                                    <td align="center"><?= $product['Price']; ?></td>
+                                    <td align="center"><?= $product['Quantity']; ?></td>
+                                    <td align="center">$<?= number_format($product['Price'] * $product['Quantity'], 2); ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            <tr>
+                                <th id="total-price" align="right" colspan="6">Total: $<?= number_format(total_price($transaction['products']),2) ?></th>
+                            </tr>
+                        </table>
+                        <p>A copy of the receipt has been sent to your email at <b><?= $transaction['cust_email'] ?></b>.</p>
+                        <p>Please keep the order number for future reference: <b><?= $transaction['orderid'] ?></b></p>
+                    </div>
                 </div>
-
             </div>
         </div>
     </div>
