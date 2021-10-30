cafd8830a802acfa303c10a972628023e6d34d89
Neo Zhen Ting <neozt99@gmail.com>
2021-10-30 16:06:44 +0800

    Style login page

--- loginpage.php
+++ loginpage.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 $login_fail = $_SESSION['recent_action'] == 'login_fail';
-echo($login_fail);
 unset($_SESSION['recent_action']);
 
 ?>
@@ -13,14 +12,15 @@ unset($_SESSION['recent_action']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="HandheldFriendly" content="true">
     <link rel="stylesheet" href="stylesheet.css">
-    <title>B&G-Registration</title>
+    <link rel="stylesheet" href="css/login-form.css">
+    <title>B&G-Login</title>
     <script type="text/javascript" src="script/messages.js"></script>
     <?php if ($login_fail): ?>
-    <script type="text/javascript">
+        <script type="text/javascript">
         window.onload = setTimeout(loginFailMsg,100);   //No idea why onload itself doesn't work on chrome, so I added some delay
     </script>
     <?php endif; ?>
-
+    
 </head>
 <body>
     <div id="container">
@@ -50,20 +50,17 @@ unset($_SESSION['recent_action']);
         <div class="content">
         <div id="pagelayout">
             <div id="rightcolumn_2">
-                <div id="registration">
-                    <br /><br /><div><h2>Beep & Geek's Member Login</h2></div>
+                <div class="inputform">
+                    <div><h2>Member Login</h2></div>
                     <form action="script/authenticate.php" method="POST">
-                            Username:<br />
-                            <input type="text" name="username"><br /><br />
-                            Password:<br />
-                            <input type="password" name="password"><br /><br />
-
-                            <input type="submit" name="submit" value="Login">
-                            <input type="reset" name="reset" value="Reset">
-                            <br /><br />
-                            <a href="registration.php">Not a member yet? Register now!</a>
+                        <label class="form-label" for="">Username</label>
+                        <input type="text" name="username" placeholder="Username" class="username">
+                        <label class="form-label" for="">Password</label>
+                        <input type="password" name="password" placeholder="Password" class="password">
+                        <input type="submit" name="submit" value="Login" class="button">
                     </form>
                 </div>
+                <p class="cta_register"><a href="registration.php">Not a member yet? Register now!</a></p>
             </div>
         </div>
 
